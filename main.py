aac62357853f8193683983b193a308740f150b59
Alex228K <156570908+Alex228K@users.noreply.github.com>
2024-02-09 20:36:46 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "РОФЛ": "Шутка"
-            "АГРО": "Злой"
-            "АГРИТЬСЯ": "Злиться или придираться"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys[word]:
-    print(meme.dict[word])
-else:
-    print('Приносим свои глубочайшие извенение перед вами, перед нашим любимым пользователем, но такое слово не найдено в нашем словаре')
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    await ctx.send(left + right)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run("")
